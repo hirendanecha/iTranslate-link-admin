e4c78d4db29d2bcc2f07df4aa8fcf06ab01a70c2
vatsal <vso.opash@gmail.com>
2024-10-02 10:21:48 +0530

    change logo and wording

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  hmr: false,
+  serverUrl: 'https://api.itanslate.link/api/v1/',
+  serverUrl2: 'http://localhost:8080/api/v1/',
+  socketUrl: 'https://api.itanslate.link/',
+  socketUrl2: 'http://localhost:8080/',
+  imgUrl: 'root/storage/uploads',
+
+};
